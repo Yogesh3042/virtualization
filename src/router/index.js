b7a276f45de378d11aabc131e517cd0664a7a64f
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2021-09-04 09:21:02 +0530

    Add route to Login

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path:'/login',
+    name:'Login',
+    component:()=>import(/*webpackChunkName:"Login"*/ '../views/Login.vue')
   }
+  
 ]
 
 const router = new VueRouter({
